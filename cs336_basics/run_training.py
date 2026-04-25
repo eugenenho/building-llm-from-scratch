340e0983e84eed26432178749bd7c6b560ed7e9b
Eugene Nho <enho@meta.com>
2026-04-24 21:28:48 -0700

    run_training.py: training loop coded added. debugging in progress

--- cs336_basics/run_training.py
+++ cs336_basics/run_training.py
@@ -7,8 +7,7 @@ import argparse
 import yaml
 from datetime import datetime
 import numpy as np
-
-
+from pathlib import Path
 
 
 if __name__ == "__main__":
@@ -64,6 +63,7 @@ if __name__ == "__main__":
     hparams = {k:v for _, group in nested_hparams.items() for k, v in group.items()}
     
     # Step 2: Check for overrides from CLI
+    list_args = ["run_name", "lr_max", "lr_min", "steps", "batch_size", "context_length", "config"]
     parser.add_argument("--run-name", default=argparse.SUPPRESS, type=str, help="name of the run")
     parser.add_argument("--lr-max", default=argparse.SUPPRESS, type=float, help="max learning rate for the lr scheduler")
     parser.add_argument("--lr-min", default=argparse.SUPPRESS, type=float, help="min learning rate for the lr scheduler")
@@ -74,22 +74,42 @@ if __name__ == "__main__":
     cli_overrides = vars(args) # dict of only what the user passed on CLI, overriding YAML
 
     for k, v in cli_overrides.items():
+        if k not in list_args:
+            raise ValueError(f"non-acceptable flag: {k}")
         if k in hparams and hparams[k] != v:
             print(f"CLI override: {k} = {v} (from YAML: {hparams[k]})")
             hparams[k] = v  
     
     # Step 3: Check if any of the fields are empty
-    missing_okay = ["run_name", "d_ff"]
+    missing_okay = ["run_name", "d_ff", "vocab_size"]
     for k, v in hparams.items():
         if v is None and k not in missing_okay: 
-            raise ValueError(f"Critical hyperparameter missing: {k: v}")
+            raise ValueError(f"Critical hyperparameter missing: {k}: {v}")
 
     if hparams["d_ff"] is None:
         hparams["d_ff"] = round(8/3 * hparams["d_model"] / 64) * 64 
     
     if hparams["run_name"] is None:
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
-        hparams["run_name"] = f"{timestamp}_lrmax{hparams["lr_max"]:.0e}_bs{hparams["batch_size"]}_steps{hparams["steps"]}"
+        hparams["run_name"] = f"{timestamp}_lrmax{hparams['lr_max']:.0e}_bs{hparams['batch_size']}_steps{hparams['steps']}"
+     
+    tokenizer = Tokenizer.from_files(                   # for sampling, for vocab_size
+        vocab_filepath = hparams["vocab_path"], 
+        merges_filepath = hparams["merges_path"], 
+        special_tokens=hparams["special_tokens"]
+    ) 
+    
+    vocab_size_from_tokenizer = len(tokenizer.vocab)
+    if hparams["vocab_size"] is not None and hparams["vocab_size"] != vocab_size_from_tokenizer:
+        print(f"vocab_size entered in YAML is incorrect. YAML value: {hparams['vocab_size']}. True value based on the loaded tokenizer: {vocab_size_from_tokenizer}. Updating to the correct value")
+    hparams["vocab_size"] = vocab_size_from_tokenizer
+
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else:
+        device = "cpu"
     
     # PART 2: CREATE A MODEL
     model = transformer_lm(
@@ -101,28 +121,23 @@ if __name__ == "__main__":
             vocab_size = hparams["vocab_size"], 
             num_layers = hparams["num_layers"],
     )
+    model.to(device)
     optimizer = AdamW(
-        params = model.parameters, 
-        lr = hparams["lr_min"], 
+        params = model.parameters(), 
+        lr = hparams["lr_max"], 
         betas = hparams["betas"], 
         weight_decay= hparams["weight_decay"], 
         eps = hparams["adamw_eps"]
     )
-
-    # PART 3: Load data
-    dataset = np.load(hparams["train_data_path"], mmap_mode='r')    
-    tokenizer = Tokenizer.from_files(vocab_filepath = hparams["vocab_path"], merges_filepath = hparams["merges_path"], special_tokens=hparams["special_tokens"]) # for sampling
+    run_dir = Path(hparams["output_dir"]) / hparams["run_name"]
+    run_dir.mkdir(parents = True, exist_ok = True)
     
-    if torch.cuda.is_available():
-        device = "cuda"
-    elif torch.backends.mps.is_available():
-      device = "mps"
-    else:
-      device = "cpu"
+    # PART 3: LOAD DATA
+    dataset = np.load(hparams["train_data_path"], mmap_mode='r')
 
-    # Training loop
-    
-    for t in hparams["steps"]:
+
+    # PART 4: TRAINING LOOP
+    for t in range(hparams["steps"]):
         
         # Get data
         inputs, targets = data_loader(                               # tensors: (batch_size, context_length)
@@ -133,11 +148,32 @@ if __name__ == "__main__":
         )       
 
         # Update learning rate
-        
+        lr = learning_rate_scheduler(
+            t=t, 
+            max_learning_rate=hparams["lr_max"],
+            min_learning_rate=hparams["lr_min"],
+            warmup_iters=hparams["warmup_iters"],
+            cosine_cycle_iters=hparams["cosine_cycle_iters"],
+        )
+        for group in optimizer.param_groups:
+            group["lr"] = lr
 
-        # Forward pass
+        # Forward, backward, step
+        optimizer.zero_grad()
         logits = model(x = inputs)                                  # output: Float[Tensor, "batch_size ... seq_len vocab_size"]
-
         loss = cross_entropy(logits = logits, targets = targets)    # int
         loss.backward()
+        gradient_clipping(model.parameters(), max_l2_norm=hparams["max_l2_norm"])
+        optimizer.step()
+
+        # Progress logging
+
+        # Checkpointing
+        if (t + 1) % hparams["save_every"] == 0:
+            ckpt_path = run_dir / f"step_{t}.pt"
+            save_checkpoint(model = model, optimizer = optimizer, iteration = t, out = ckpt_path)
+
+        
+
+
         
